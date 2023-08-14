b09e0a96c39012590275e1aded8aec229be29d39
reolinodev <reolino.dev@gmail.com>
2023-08-14 11:38:26 +0900

    - 포트 및 DB 정보 변경 - 테스트 코드 변경

--- src/test/java/com/back/support/CryptUtilsTest.java
+++ src/test/java/com/back/support/CryptUtilsTest.java
@@ -1,11 +1,8 @@
 package com.back.support;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import com.back.domain.User;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,17 +11,23 @@ class CryptUtilsTest {
 
     @Test
     void encrypt() throws IOException {
-        String str = "test";
+        String str = "bdrnd";
         String ecnryptStr = CryptUtils.encrypt(str);
-        System.out.println("=="+ecnryptStr);
+        System.out.println(ecnryptStr);
     }
 
     @Test
     void encrypt256() throws IOException, NoSuchAlgorithmException {
         String str = "1234";
         String ecnryptStr = CryptUtils.encryptSha256(str);
-        System.out.println("=="+ecnryptStr);
+        System.out.println(ecnryptStr);
     }
 
-
+    @Test
+    void decrypt() throws IOException {
+        String str = "";
+        String decryptStr = CryptUtils.decrypt(str);
+        System.out.println(decryptStr);
+    }
 }
+
